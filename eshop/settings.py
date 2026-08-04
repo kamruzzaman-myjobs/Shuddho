b9860c1490c74c03f79998a58bb167ab419708d7
Md Kamruzzman <81611516+kamruzzaman-myjobs@users.noreply.github.com>
2026-08-04 16:18:37 +0600

    Specify .env file path in settings
    Update environment variable loading to specify .env file path.

--- eshop/settings.py
+++ eshop/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 import cloudinary
 
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
